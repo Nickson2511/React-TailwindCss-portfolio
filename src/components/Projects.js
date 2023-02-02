ffd3fefbdb46ec636df269ccd125207784dab7c2
Nickson Okwemba <okwembanickson8@gmail.com>
2023-02-02 04:32:14 +0300

    React

---
+++ src/components/Projects.js
@@ -0,0 +1,50 @@
+import { CodeIcon } from "@heroicons/react/solid";
+import React from "react";
+import { projects } from "../data";
+
+export default function Projects() {
+  return (
+    <section id="projects" className="text-gray-400 bg-gray-900 body-font">
+      <div className="container px-5 py-10 mx-auto text-center lg:px-40">
+        <div className="flex flex-col w-full mb-20">
+          <CodeIcon className="mx-auto inline-block w-10 mb-4" />
+          <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-white">
+            The projects i have build:
+          </h1>
+          <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
+            With the help of HTML and CSS, i have built several responsive landing pages which <br/>
+            posses user interface that allows assured human computer interface. 
+            I have designed a complete 
+            User registration websites that allows signups authentication and logins pages. I am glad to receive this <br/>
+            abundance grace from God since He feeds me with every knowledge and wisdom i apply in creating staffs
+          </p>
+        </div>
+        <div className="flex flex-wrap -m-4">
+          {projects.map((project) => (
+            <a
+              href={project.link}
+              key={project.image}
+              className="sm:w-1/2 w-100 p-4">
+              <div className="flex relative">
+                <img
+                  alt="gallery"
+                  className="absolute inset-0 w-full h-full object-cover object-center"
+                  src={project.image}
+                />
+                <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
+                  <h2 className="tracking-widest text-sm title-font font-medium text-green-400 mb-1">
+                    {project.subtitle}
+                  </h2>
+                  <h1 className="title-font text-lg font-medium text-white mb-3">
+                    {project.title}
+                  </h1>
+                  <p className="leading-relaxed">{project.description}</p>
+                </div>
+              </div>
+            </a>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ src/data.js
@@ -0,0 +1,60 @@
+export const projects = [
+  {
+    title: "React Reserve",
+    subtitle: "MERN Stack",
+    description:
+      "Hosting projects is a common idea i learn't from my favorite mentor, perhaps i opened github, ?",
+    image: "./project-1.gif",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "React Tracks",
+    subtitle: "React and Python",
+    description:
+      "Using Django to build Rest APIs is my best part, acting as a serverside framework?",
+    image: "./project-2.gif",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "DevChat",
+    subtitle: "React and Firebase",
+    description:
+      "Enriching already grown platforms like Dev communities, Stack overflows lets me exchange ideas with others?",
+    image: "./project-3.gif",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "Epic Todo App",
+    subtitle: "React Hooks",
+    description:
+      "an enough project that can land your first dream job software development?",
+    image: "./project-4.gif",
+    link: "https://reactbootcamp.com",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Building with react is a game changing plan. It is a language that you can master and build real world projects.",
+    image: "https://scontent.fnbo8-1.fna.fbcdn.net/v/t39.30808-6/277098481_294479929473400_5984747071384375728_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=8bfeb9&_nc_eui2=AeEnsCtaWadwFYU9yEE0x5ysf0UGexl5d35_RQZ7GXl3fqZ5UrPJxDSXNxIqDCQBz9d7RXGY1FjqCyfMjYAvIvOB&_nc_ohc=Yz1QbL9leq8AX_0pL59&_nc_ht=scontent.fnbo8-1.fna&oh=00_AfBabMYDRI7wmPTPuQ2Q0dScVaGNZU__H-HY8XNCaihnzw&oe=63DF54C5",
+    name: "nick okwesh",
+   company: "Mount Kenya University",
+  },
+  {
+    quote:
+      "Appreciating the dev community for the support of knowledge and information at the right way, iam now cofident with my projects.",
+    image: "https://scontent.fnbo8-1.fna.fbcdn.net/v/t39.30808-6/241455421_171485861772808_4947804640126538999_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=8bfeb9&_nc_eui2=AeHzS-d7VXO7UTqagRDlc9J2egkIQkAC_fl6CQhCQAL9-WYyRX3jCcQs3jbAaODHEfZmWaMAOG_76lGMh5KQdi3A&_nc_ohc=bFLmUDZkNm4AX8cQ46E&_nc_ht=scontent.fnbo8-1.fna&oh=00_AfB87e78WPjphnDHaDJ6NwIh6qxMHNSQXtQ0naMCljjlVA&oe=63DF10E9",
+    name: "Nickson Okwemba",
+    company: "Dooble.io",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "Next.js",
+  "Node",
+  "GraphQL",
+  "Material UI",
+];
